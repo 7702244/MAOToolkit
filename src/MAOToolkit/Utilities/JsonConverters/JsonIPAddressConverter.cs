bb57aa2bff5c8b711931c2a33305cea3431cce5d
7702244 <56172899+7702244@users.noreply.github.com>
2023-02-05 00:10:11 +0200

    Add JsonIPAddressConverter

---
+++ src/MAOToolkit/Utilities/JsonConverters/JsonIPAddressConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace MAOToolkit.Utilities.JsonConverters
+{
+    public class JsonIPAddressConverter : JsonConverter<IPAddress>
+    {
+        public override IPAddress? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (IPAddress.TryParse(reader.GetString(), out IPAddress? result))
+            {
+                return result;
+            }
+
+            return null;
+        }
+
+        public override void Write(Utf8JsonWriter writer, IPAddress value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString());
+        }
+    }
+}
