6b97b6a2dcff6c1f3b99bf4a7adc501c80d21512
7702244 <56172899+7702244@users.noreply.github.com>
2023-08-04 13:27:03 +0300

    Add OrderBy string expression

--- src/MAOToolkit/Extensions/QueryableExtensions.cs
+++ src/MAOToolkit/Extensions/QueryableExtensions.cs
@@ -1,9 +1,24 @@
 using System.Linq.Expressions;
+using System.Reflection;
 
 namespace MAOToolkit.Extensions
 {
     public static class QueryableExtensions
     {
+        public static IOrderedQueryable<T> OrderBy<T>(this IQueryable<T> queryable, string orderByProperty, bool desc = false)
+        {
+            string command = desc ? "OrderByDescending" : "OrderBy";
+            var type = typeof(T);
+            var property = type.GetProperty(orderByProperty, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+            ArgumentNullException.ThrowIfNull(property);
+
+            var parameter = Expression.Parameter(type, "p");
+            var propertyAccess = Expression.MakeMemberAccess(parameter, property);
+            var orderByExpression = Expression.Lambda(propertyAccess, parameter);
+            var resultExpression = Expression.Call(typeof(Queryable), command, new Type[] { type, property.PropertyType }, queryable.Expression, Expression.Quote(orderByExpression));
+            return (IOrderedQueryable<T>)queryable.Provider.CreateQuery<T>(resultExpression);
+        }
+
         public static IQueryable<T> WhereAny<T>(this IQueryable<T> queryable, params Expression<Func<T, bool>>[] predicates)
         {
             var parameter = Expression.Parameter(typeof(T));
