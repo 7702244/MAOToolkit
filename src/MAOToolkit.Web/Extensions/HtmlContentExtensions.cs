39d90916c70c1a3ed518141ffb1928369f8fa6fc
7702244 <56172899+7702244@users.noreply.github.com>
2025-01-14 18:22:54 +0200

    Modify HtmlEncoder in HtmlContentExtensions

--- src/MAOToolkit.Web/Extensions/HtmlContentExtensions.cs
+++ src/MAOToolkit.Web/Extensions/HtmlContentExtensions.cs
@@ -1,15 +1,18 @@
 using System.Text.Encodings.Web;
+using System.Text.Unicode;
 using Microsoft.AspNetCore.Html;
 
 namespace MAOToolkit.Extensions
 {
     public static class HtmlContentExtensions
     {
+        private static readonly HtmlEncoder UnicodeHtmlEncoder = HtmlEncoder.Create(UnicodeRanges.All);
+        
         public static string GetString(this IHtmlContent content)
         {
             using (var writer = new StringWriter())
             {
-                content.WriteTo(writer, HtmlEncoder.Default);
+                content.WriteTo(writer, UnicodeHtmlEncoder);
                 return writer.ToString();
             }
         }
