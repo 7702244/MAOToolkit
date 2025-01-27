9a7327f9bc3eb7b6d789b9ee93b4d8774c97b042
7702244 <56172899+7702244@users.noreply.github.com>
2025-01-27 22:35:49 +0200

    Zip fixes

--- src/MAOToolkit/Extensions/ZipArchiveExtensions.cs
+++ src/MAOToolkit/Extensions/ZipArchiveExtensions.cs
@@ -4,29 +4,27 @@ namespace MAOToolkit.Extensions
 {
     public static class ZipArchiveExtensions
     {
-        public static void CreateEntryFromAny(this ZipArchive archive, string sourcePath, string entryName = "")
+        public static void CreateEntryFromAny(this ZipArchive archive, string sourcePath, string entryName = "", CompressionLevel compressionLevel = CompressionLevel.Optimal)
         {
             string fileName = Path.GetFileName(sourcePath);
             string entryFullName = Path.Combine(entryName, fileName);
             
             if (File.GetAttributes(sourcePath).HasFlag(FileAttributes.Directory))
             {
-                archive.CreateEntryFromDirectory(sourcePath, entryFullName);
+                archive.CreateEntryFromDirectory(sourcePath, entryFullName, compressionLevel);
             }
             else
             {
-                archive.GetEntry(entryFullName)?.Delete();
-                
-                archive.CreateEntryFromFile(sourcePath, entryFullName, CompressionLevel.Optimal);
+                archive.CreateEntryFromFile(sourcePath, entryFullName, compressionLevel);
             }
         }
 
-        public static void CreateEntryFromDirectory(this ZipArchive archive, string sourceDirName, string entryName = "")
+        public static void CreateEntryFromDirectory(this ZipArchive archive, string sourceDirName, string entryName = "", CompressionLevel compressionLevel = CompressionLevel.Optimal)
         {
             var files = Directory.EnumerateFileSystemEntries(sourceDirName);
             foreach (string file in files)
             {
-                archive.CreateEntryFromAny(file, entryName);
+                archive.CreateEntryFromAny(file, entryName, compressionLevel);
             }
         }
     }
