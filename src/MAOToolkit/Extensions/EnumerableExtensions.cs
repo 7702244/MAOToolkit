f187414b1cb6f67d88eefe6ab3fb9d1dcd3f15b9
7702244 <56172899+7702244@users.noreply.github.com>
2025-04-06 21:28:20 +0300

    add enumerable dynamic order by

--- src/MAOToolkit/Extensions/EnumerableExtensions.cs
+++ src/MAOToolkit/Extensions/EnumerableExtensions.cs
@@ -1,3 +1,6 @@
+using System.Linq.Expressions;
+using System.Reflection;
+
 namespace MAOToolkit.Extensions
 {
     public static class EnumerableExtensions
@@ -14,6 +17,32 @@ namespace MAOToolkit.Extensions
                 act(element);
             }
             return source;
+        }
+        
+        public static IOrderedEnumerable<T> OrderBy<T>(this IEnumerable<T> source, string orderByProperty, bool desc = false)
+        {
+            string methodName = desc ? "OrderByDescending" : "OrderBy";
+            var type = typeof(T);
+            var property = type.GetProperty(orderByProperty, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+            ArgumentNullException.ThrowIfNull(property);
+
+            var parameter = Expression.Parameter(type, "p");
+            var propertyAccess = Expression.MakeMemberAccess(parameter, property);
+            var orderByExpression = Expression.Lambda(propertyAccess, parameter);
+            
+            object? result = typeof(Enumerable).GetMethods().Single(method => method.Name == methodName
+                && method.IsGenericMethodDefinition
+                && method.GetGenericArguments().Length == 2
+                && method.GetParameters().Length == 2)
+            .MakeGenericMethod(typeof(T), property.PropertyType)
+            .Invoke(null, [source, orderByExpression.Compile()]);
+
+            if (result is null)
+            {
+                throw new InvalidOperationException();
+            }
+            
+            return (IOrderedEnumerable<T>)result;
         }
     }
 }
