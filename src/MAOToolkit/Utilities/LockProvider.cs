f7e5d4bf834b91de2535492ba871a8364c8dfe15
7702244 <56172899+7702244@users.noreply.github.com>
2025-05-26 23:24:08 +0300

    Rewrite LockProvider.cs

--- src/MAOToolkit/Utilities/LockProvider.cs
+++ src/MAOToolkit/Utilities/LockProvider.cs
@@ -2,248 +2,71 @@ using System.Collections.Concurrent;
 
 namespace MAOToolkit.Utilities
 {
-    /// <summary>
-    /// A LockProvider based upon the SemaphoreSlim class to selectively lock objects, resources or statement blocks 
-    /// according to given unique IDs in a sync or async way.
-    /// 
-    /// SAMPLE USAGE & ADDITIONAL INFO:
-    /// - https://www.ryadel.com/en/asp-net-core-lock-threads-async-custom-ids-lockprovider/
-    /// - https://github.com/Darkseal/LockProvider/
-    /// </summary>
-    public class LockProvider<T> where T : notnull
+    public static class LockProvider
     {
-        static readonly LazyConcurrentDictionary<T, InnerSemaphore> LockDictionary = new();
+        private static readonly ConcurrentDictionary<object, AsyncLock> _locks = new();
 
-        public LockProvider() { }
-
-        /// <summary>
-        /// Blocks the current thread (according to the given ID) until it can enter the LockProvider
-        /// </summary>
-        /// <param name="idToLock">the unique ID to perform the lock</param>
-        public void Wait(T idToLock)
+        public static async Task<IDisposable> LockAsync(object key, CancellationToken ct = default)
         {
-            LockDictionary.GetOrAdd(idToLock, new InnerSemaphore(1, 1)).Wait();
+            var asyncLock = _locks.GetOrAdd(key, _ => new AsyncLock());
+            var lockHandle = await asyncLock.AcquireAsync(ct).ConfigureAwait(false);
+            return new AsyncLockRelease(() => Release(key, asyncLock, lockHandle));
         }
 
-        /// <summary>
-        /// Blocks the current thread (according to the given ID) until it can enter the LockProvider
-        /// </summary>
-        /// <param name="idToLock">the unique ID to perform the lock</param>
-        /// <param name="millisecondsTimeout"></param>
-        public bool Wait(T idToLock, int millisecondsTimeout)
+        private static void Release(object key, AsyncLock asyncLock, IDisposable lockHandle)
         {
-            var semaphore = LockDictionary.GetOrAdd(idToLock, new InnerSemaphore(1, 1));
-            if (!semaphore.Wait(millisecondsTimeout))
+            try
             {
-                if (!semaphore.HasWaiters && LockDictionary.TryRemove(idToLock, out _))
-                    semaphore.Dispose();
-                return false;
+                lockHandle.Dispose();
+                if (asyncLock.IsFree)
+                {
+                    _locks.TryRemove(key, out _);
+                }
             }
-            return true;
-        }
-
-        /// <summary>
-        /// Blocks the current thread (according to the given ID) until it can enter the LockProvider
-        /// </summary>
-        /// <param name="idToLock">the unique ID to perform the lock</param>
-        /// <param name="millisecondsTimeout"></param>
-        /// <param name="token"></param>
-        public bool Wait(T idToLock, int millisecondsTimeout, CancellationToken token)
-        {
-            var semaphore = LockDictionary.GetOrAdd(idToLock, new InnerSemaphore(1, 1));
-            if (!semaphore.Wait(millisecondsTimeout, token))
+            catch
             {
-                if (!semaphore.HasWaiters && LockDictionary.TryRemove(idToLock, out _))
-                    semaphore.Dispose();
-                return false;
-            }
-            return true;
-        }
-
-        /// <summary>
-        /// Asynchronously puts thread to wait (according to the given ID) until it can enter the LockProvider
-        /// </summary>
-        /// <param name="idToLock">the unique ID to perform the lock</param>
-        public async Task WaitAsync(T idToLock)
-        {
-            await LockDictionary.GetOrAdd(idToLock, new InnerSemaphore(1, 1)).WaitAsync();
-        }
-
-        /// <summary>
-        /// Asynchronously puts thread to wait (according to the given ID) until it can enter the LockProvider
-        /// </summary>
-        /// <param name="idToLock">the unique ID to perform the lock</param>
-        /// <param name="millisecondsTimeout"></param>
-        public async Task<bool> WaitAsync(T idToLock, int millisecondsTimeout)
-        {
-            var semaphore = LockDictionary.GetOrAdd(idToLock, new InnerSemaphore(1, 1));
-            if (!await semaphore.WaitAsync(millisecondsTimeout))
-            {
-                if (!semaphore.HasWaiters && LockDictionary.TryRemove(idToLock, out _))
-                    semaphore.Dispose();
-                return false;
-            }
-            return true;
-        }
-
-        /// <summary>
-        /// Asynchronously puts thread to wait (according to the given ID) until it can enter the LockProvider
-        /// </summary>
-        /// <param name="idToLock">the unique ID to perform the lock</param>
-        /// <param name="millisecondsTimeout"></param>
-        /// <param name="token"></param>
-        public async Task<bool> WaitAsync(T idToLock, int millisecondsTimeout, CancellationToken token)
-        {
-            var semaphore = LockDictionary.GetOrAdd(idToLock, new InnerSemaphore(1, 1));
-            if (!await semaphore.WaitAsync(millisecondsTimeout, token))
-            {
-                if (!semaphore.HasWaiters && LockDictionary.TryRemove(idToLock, out _))
-                    semaphore.Dispose();
-                return false;
-            }
-            return true;
-        }
-
-        public void Release(T idToUnlock)
-        {
-            if (LockDictionary.TryGetValue(idToUnlock, out var semaphore)
-                && semaphore is not null)
-            {
-                semaphore.Release();
-                if (!semaphore.HasWaiters && LockDictionary.TryRemove(idToUnlock, out _))
-                    semaphore.Dispose();
+                // Игнорируем ошибки при освобождении
             }
         }
-    }
 
-    public class InnerSemaphore : IDisposable
-    {
-        private readonly SemaphoreSlim _semaphore;
-        private int _waiters;
-
-        public InnerSemaphore(int initialCount, int maxCount)
+        private class AsyncLock
         {
-            _semaphore = new SemaphoreSlim(initialCount, maxCount);
-            _waiters = 0;
-        }
+            private readonly SemaphoreSlim _semaphore = new(1, 1);
+            private int _refCount;
 
-        public void Wait()
-        {
-            Interlocked.Increment(ref _waiters);
-            _semaphore.Wait();
-        }
+            public bool IsFree => _refCount == 0;
 
-        public bool Wait(int millisecondsTimeout)
-        {
-            Interlocked.Increment(ref _waiters);
-            if (!_semaphore.Wait(millisecondsTimeout))
+            public async Task<IDisposable> AcquireAsync(CancellationToken ct)
             {
-                Interlocked.Decrement(ref _waiters);
-                return false;
+                await _semaphore.WaitAsync(ct).ConfigureAwait(false);
+                Interlocked.Increment(ref _refCount);
+                return new LockHandle(this);
             }
-            return true;
-        }
 
-        public void Wait(CancellationToken token)
-        {
-            Interlocked.Increment(ref _waiters);
-            _semaphore.Wait(token);
-        }
-
-        public bool Wait(int millisecondsTimeout, CancellationToken token)
-        {
-            Interlocked.Increment(ref _waiters);
-            if (!_semaphore.Wait(millisecondsTimeout, token))
+            private class LockHandle(AsyncLock parent) : IDisposable
             {
-                Interlocked.Decrement(ref _waiters);
-                return false;
+                private int _disposed;
+
+                public void Dispose()
+                {
+                    if (Interlocked.Exchange(ref _disposed, 1) == 0)
+                    {
+                        Interlocked.Decrement(ref parent._refCount);
+                        parent._semaphore.Release();
+                    }
+                }
             }
-            return true;
         }
 
-        public async Task WaitAsync()
+        private class AsyncLockRelease(Action release) : IDisposable
         {
-            Interlocked.Increment(ref _waiters);
-            await _semaphore.WaitAsync();
-        }
+            private int _disposed;
 
-        public async Task<bool> WaitAsync(int millisecondsTimeout)
-        {
-            Interlocked.Increment(ref _waiters);
-            if (!await _semaphore.WaitAsync(millisecondsTimeout))
+            public void Dispose()
             {
-                Interlocked.Decrement(ref _waiters);
-                return false;
+                if (Interlocked.Exchange(ref _disposed, 1) == 0)
+                    release();
             }
-            return true;
-        }
-
-        public async Task WaitAsync(CancellationToken token)
-        {
-            Interlocked.Increment(ref _waiters);
-            await _semaphore.WaitAsync(token);
-        }
-
-        public async Task<bool> WaitAsync(int millisecondsTimeout, CancellationToken token)
-        {
-            Interlocked.Increment(ref _waiters);
-            if (!await _semaphore.WaitAsync(millisecondsTimeout, token))
-            {
-                Interlocked.Decrement(ref _waiters);
-                return false;
-            }
-            return true;
-        }
-
-        public void Release()
-        {
-            _semaphore.Release();
-            Interlocked.Decrement(ref _waiters);
-        }
-
-        public void Dispose()
-        {
-            _semaphore?.Dispose();
-            GC.SuppressFinalize(this);
-        }
-
-        public bool HasWaiters => _waiters > 0;
-    }
-
-    public class LazyConcurrentDictionary<TKey, TValue>
-        where TKey : notnull
-    {
-        private readonly ConcurrentDictionary<TKey, Lazy<TValue>> _concurrentDictionary;
-
-        public LazyConcurrentDictionary()
-        {
-            _concurrentDictionary = new ConcurrentDictionary<TKey, Lazy<TValue>>();
-        }
-
-        public TValue GetOrAdd(TKey key, TValue value)
-        {
-            var lazyResult = _concurrentDictionary.GetOrAdd(key, k => new Lazy<TValue>(() => value, LazyThreadSafetyMode.ExecutionAndPublication));
-            return lazyResult.Value;
-        }
-
-        public TValue GetOrAdd(TKey key, Func<TKey, TValue> valueFactory)
-        {
-            var lazyResult = _concurrentDictionary.GetOrAdd(key, k => new Lazy<TValue>(() => valueFactory(k), LazyThreadSafetyMode.ExecutionAndPublication));
-            return lazyResult.Value;
-        }
-
-        public bool TryGetValue(TKey key, out TValue? value)
-        {
-            var success = _concurrentDictionary.TryGetValue(key, out var lazyResult);
-            value = success && lazyResult is not null ? lazyResult.Value : default;
-            return success;
-        }
-
-        public bool TryRemove(TKey key, out TValue? value)
-        {
-            var success = _concurrentDictionary.TryRemove(key, out var lazyResult);
-            value = success && lazyResult is not null ? lazyResult.Value : default;
-            return success;
         }
     }
 }
